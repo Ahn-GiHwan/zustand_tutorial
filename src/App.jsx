c49cb778d9984e501ab004abef11e2a62465066d
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-14 03:16:24 +0900

    feat: Use Input & InputInfo component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Input from "./components/Input";
+import InputInfo from "./components/InputInfo";
+
+function App() {
+  return (
+    <>
+      <Input />
+      <InputInfo />
+    </>
+  );
+}
+
+export default App;
