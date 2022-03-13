011774f3d9eccb82a30d06718165640b5eff3b94
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-14 03:20:52 +0900

    feat: Use InputView and InputLength Component

---
+++ src/components/InputInfo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import InputLength from "./InputLength";
+import InputView from "./InputView";
+
+const InputInfo = () => {
+  return (
+    <>
+      <InputView />
+      <InputLength />
+    </>
+  );
+};
+
+export default InputInfo;
